aef74ae576bcb811f7a3e20ac131a06f81e631a1
Žiga Krašovec <zkrasovec@gmail.com>
2020-07-29 12:36:56 +0200

    Added project and dev dependencies. Linting rules setup.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+        endOfLine: 'lf',
+        semi: true,
+        tabWidth: 2,
+      },
+    ],
+  },
+  plugins: ['react', 'prettier'],
+};
