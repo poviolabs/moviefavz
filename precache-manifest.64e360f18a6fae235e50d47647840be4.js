ff76a1df2d7cd56210cd812ea98edfac53bf7e53
Adam Giacomelli <adam.giacom@gmail.com>
2020-08-03 13:33:15 +0200

    Updates

---
+++ precache-manifest.64e360f18a6fae235e50d47647840be4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ca5bf5788815d371247e5b6a62c2ee",
+    "url": "/poviolabs.github.io/moviefavz/index.html"
+  },
+  {
+    "revision": "4eba6182b50e8799d9b6",
+    "url": "/poviolabs.github.io/moviefavz/static/css/main.e2af1c4b.chunk.css"
+  },
+  {
+    "revision": "08cc442e3b816eb0447b",
+    "url": "/poviolabs.github.io/moviefavz/static/js/2.3b55494e.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/poviolabs.github.io/moviefavz/static/js/2.3b55494e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eba6182b50e8799d9b6",
+    "url": "/poviolabs.github.io/moviefavz/static/js/main.e8453205.chunk.js"
+  },
+  {
+    "revision": "5f66cf3948aa02f96b59",
+    "url": "/poviolabs.github.io/moviefavz/static/js/runtime-main.e596c330.js"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/poviolabs.github.io/moviefavz/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/poviolabs.github.io/moviefavz/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/poviolabs.github.io/moviefavz/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/poviolabs.github.io/moviefavz/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "b58757394ca4054e387fd8c8d14cde7d",
+    "url": "/poviolabs.github.io/moviefavz/static/media/home-banner-bg-2.b5875739.jpg"
+  }
+]);
